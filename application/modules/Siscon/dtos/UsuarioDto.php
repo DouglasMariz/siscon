9eceb083b41cfcc484a5fe521b59e4a165cb7ef9
Douglas Mariz <douglasviviane@gmail.com>
2015-06-05 17:26:44 -0300

    Tudo novo pelo linux

---
+++ application/modules/Siscon/dtos/UsuarioDto.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Douglas Mariz
+ * Date: 21/02/2015
+ * Time: 20:32
+ */
+
+class Siscon_UsuarioDto {
+    /**
+     * Armazena o cpf do usuário
+     *
+     * @access protected
+     * @name cpf
+     * @var integer
+     * @property cpf
+     */
+    protected $cpf;
+
+    /**
+     * Armazena o email
+     *
+     * @access protected
+     * @name email
+     * @var string
+     * @property email
+     */
+    protected $email;
+
+    /**
+     * Armazena o login
+     *
+     * @access protected
+     * @name login
+     * @var string
+     * @property login
+     */
+    protected $login;
+
+    /**
+     * Armazena o senha
+     *
+     * @access protected
+     * @name senha
+     * @var string
+     * @property senha
+     */
+    protected $senha;
+
+    /**
+     * Armazena o nome
+     *
+     * @access protected
+     * @name nome
+     * @var string
+     * @property nome
+     */
+    protected $nome;
+
+} 
---
+++ application/modules/Siscon/dtos/testeDto.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Douglas Mariz
+ * Date: 12/04/2015
+ * Time: 10:07
+ */
+
+
+class Siscon_testeDto {
+    /**
+     * Armazena o cpf do usuário
+     *
+     * @access protected
+     * @name cpf
+     * @var integer
+     * @property cpf
+     */
+    protected $cpf;
+
+    /**
+     * Armazena o email
+     *
+     * @access protected
+     * @name email
+     * @var string
+     * @property email
+     */
+    protected $email;
+}
